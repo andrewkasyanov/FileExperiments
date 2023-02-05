b961a019a63cbe47a2feb5598231341d73057ef1
Andrei Kasianov <kasyanov.rss@gmail.com>
2023-02-05 20:49:17 +0300

    write

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\kasya\\Desktop\\11\\22.txt";
+//        File file = new File(path);
+
+        try {
+            String data = Files.readString(Paths.get(path));
+            System.out.println(data);
+
+            Files.writeString(
+                    Paths.get(path),
+                    "\ntest testtest!!!",
+                    StandardOpenOption.APPEND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
