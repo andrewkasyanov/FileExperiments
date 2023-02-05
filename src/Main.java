b4b9865d6a5422026b851f5d19b6113c881e637c
Andrei Kasianov <kasyanov.rss@gmail.com>
2023-02-05 21:53:01 +0300

    archive

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
+import java.awt.*;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "C:\\Users\\kasya\\Desktop\\11\\22.txt";
-//        File file = new File(path);
+        String path = "C:\\Users\\kasya\\Desktop\\11\\";
+        String in = path + "22.txt";
+        String out = path + "archive.zip";
 
         try {
-            String data = Files.readString(Paths.get(path));
-            System.out.println(data);
-
-            Files.writeString(
-                    Paths.get(path),
-                    "\ntest testtest!!!",
-                    StandardOpenOption.APPEND);
-
+            FileOutputStream outputStream = new FileOutputStream(out);
+            ZipOutputStream zipOut = new ZipOutputStream(outputStream);
+            ZipEntry entry = new ZipEntry(new File(in).getName());
+            zipOut.putNextEntry(entry);
+            byte[] data = Files.readAllBytes(Paths.get(in));
+            zipOut.write(data);
+            zipOut.flush();
+            zipOut.close();
+            outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+
 
     }
 }
